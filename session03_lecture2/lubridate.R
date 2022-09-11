6fee64d1d1407bf01a487c2e393cb7ef79a2c3ee
Kostis Christodoulou <40967414+kostis-christodoulou@users.noreply.github.com>
2022-09-11 14:31:37 +0100

    sessions 1-4

---
+++ session03_lecture2/lubridate.R
@@ -0,0 +1,17 @@
+library(lubridate)
+
+date <- ymd("2022-12-25")
+today <- Sys.Date() #reads system's current date
+
+year(date)
+month(date) # Equal to 12
+month(date,label = TRUE) # Equal to "Dec"
+
+day(date)
+wday(date,label = TRUE) # Equal to "Wed"
+
+isoweek(date)
+
+# can calculate how many days it is until Xmas
+date - today 
+
